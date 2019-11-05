26a47ab1d167f6974b40eaeeb4227d6e6da9d117
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-11-05 09:24:18 +0100

    Added missing requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module no.ssb.mutex.gcs {
     requires google.cloud.core;
     requires google.cloud.storage;
     requires com.google.auth.oauth2;
+    requires com.google.auth;
 
     exports no.ssb.locking;
 }
